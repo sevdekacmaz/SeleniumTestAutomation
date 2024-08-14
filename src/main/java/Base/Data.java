28055822e757d59ed20943123e30125378c87d84
sevdekacmaz <sevdekacmz@gmail.com>
2024-08-14 21:32:24 +0300

    add login case

---
+++ src/main/java/Base/Data.java
@@ -0,0 +1,10 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+
+public class Data {
+
+    public static WebDriver driver;
+    public String url = "https://www.netflix.com/login";
+
+}
---
+++ src/main/java/Pages/HomePage.java
@@ -0,0 +1,16 @@
+package Pages;
+
+import Base.Data;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
+public class HomePage extends Data {
+
+    public HomePage loginController(){
+
+       String value =  driver.findElement(By.cssSelector("[data-uia='nmhp-card-hero-text-welcome']")).getText();
+        Assert.assertEquals("Welcome back!", value);
+        return this;
+    }
+
+}
