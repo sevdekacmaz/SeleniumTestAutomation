591afaf86db212c931dea52538f1f9d03ec4604c
Cevdet Ayaz <cevdetayaz@gmail.com>
2024-08-16 21:52:21 +0300

    sytem get property eklendi url

--- src/main/java/Base/Data.java
+++ src/main/java/Base/Data.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.WebDriver;
 public class Data {
 
     public static WebDriver driver;
-    public String url = "https://www.netflix.com/login";
+    //public String url = "https://www.netflix.com/login";
+    public String url = System.getProperty("url");
     public String message = "Your password must contain between 4 and 60 characters.";
     public String message2 = "Incorrect password for sevde@gmail.com";
     public String message3 = "Please enter a valid email.";
