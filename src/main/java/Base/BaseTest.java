4c5adbc3e00d360cbdceb5bfde835cd02e730b6c
Cevdet Ayaz <cevdetayaz@gmail.com>
2024-08-16 21:37:00 +0300

    sytem get property eklendi

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -2,13 +2,26 @@ package Base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest extends Data {
     @BeforeMethod
     public void openBrowser(){
-        driver = new ChromeDriver();
+        //String tarayici = "firefox";
+        String tarayici = System.getProperty("browser");
+        if (tarayici.equals("chrome")) {
+            driver = new ChromeDriver();
+        }
+        else if (tarayici.equals("firefox")) {
+            driver = new FirefoxDriver();
+        }
+        else if (tarayici.equals("edge"))
+            driver = new EdgeDriver();
+
+
         driver.get(url);
     }
 
