ae8b3ddcaaf19821a07b3f0723a62d6c4ecec0ae
Cevdet Ayaz <cevdetayaz@gmail.com>
2024-08-16 21:27:33 +0300

    ingilizce sayfaya göre mesajlar düzenlendi

--- src/main/java/Base/Data.java
+++ src/main/java/Base/Data.java
@@ -6,9 +6,9 @@ public class Data {
 
     public static WebDriver driver;
     public String url = "https://www.netflix.com/login";
-    public String message = "Parolanız 4 ila 60 karakter olmalıdır.";
-    public String message2 = "sevde@gmail.com için parola yanlış";
-    public String message3 = "Lütfen geçerli bir e‑posta adresi girin.";
+    public String message = "Your password must contain between 4 and 60 characters.";
+    public String message2 = "Incorrect password for sevde@gmail.com";
+    public String message3 = "Please enter a valid email.";
 
 
 
